14654ea2c9746175d79f8b3e768514f20759f5e7
aleenakrish <aleenakrish59@gmail.com>
2024-11-23 17:53:08 +0530

    saved

--- appwrte/lib/homepage.dart
+++ appwrte/lib/homepage.dart
@@ -1,3 +1,7 @@
+
+import 'package:appwrite/models.dart';
+import 'package:appwrte/services/appwriteservice.dart';
+import 'package:appwrte/todo.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -10,30 +14,98 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
 
   TextEditingController _controller = TextEditingController();
-  late AppwriteService _appwriteService
+  late AppwriteService _appwriteService;
+  List? _task;
+
+
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _appwriteService = AppwriteService();
+    _loadTasks();
+    _task =[];
+  }
+
+  Future<void>_addtask()async{
+    final task = _controller.text;
+
+    if(task.isNotEmpty){
+      try{
+        await _appwriteService.addTask(task);
+        _controller.clear();
+
+      }catch(e){
+        print(e);
+      }
+    }
+  }
+
+
+  Future<void>_loadTasks()async{
+    try {
+      final tasks= await _appwriteService.getTask();
+      setState(() {
+        _task=tasks.map((e)=>Task.formDocument(e)).toList();
+        print("###############################################");
+      });
+    } catch (e) {
+      print(e);
+      
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todolist"),
+        title: Text("TodoList"),
       ),
       body: Container(
-       child: ListView(
-        children: [
-          Row(
-            children: [
-              Container(
-                child: Expanded(child: TextField(
-                  
-                )
+       child: 
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      // margin: EdgeInsets.only(left: 20,right: 50),
+                      child: Expanded(child: TextField(controller: _controller,
+                        
+                      )
+                      ),
+                    ),
+                   ElevatedButton(onPressed: (){
+                      _addtask();
+                   }, child: Text("add"))
+                  ],
                 ),
-              ),
-              Container(child: TextButton(onPressed: (){}, child: Text("add")),)
-            ],
-          )
-        ],
-       ),
+                Container(
+                  child: Expanded(child: ListView.builder(
+                                
+                    itemCount: _task!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onLongPress: () {
+                          final documentId=_task![index].id;
+                          print(documentId);
+                          _appwriteService.delete(documentId);
+                          _loadTasks();
+                        },
+                        title: Text(_task![index].task.toString()),
+                      );
+                    },)
+                    ),
+                )
+              ],
+            ),
+          ),
+          
+          
+       
       ),
     );
   }
--- appwrte/lib/services/appwriteservice.dart
+++ appwrte/lib/services/appwriteservice.dart
@@ -3,21 +3,66 @@ import 'package:appwrite/models.dart';
 
 
 
-class Appwriteservice {
+class AppwriteService {
   late Client client;
   late Databases databases;
   static const endpoit="https://cloud.appwrite.io/v1";
   static const projectid="674065a5000731fa751c";
-  static const DatabaseId="6740678c00383d6cb974";
+  static const databaseId="6740678c00383d6cb974";
   static const collectionId="6740679e002b141af8fa";
 
 
-  Appwriteservice(){
+  AppwriteService(){
     client=Client();
     client.setEndpoint(endpoit);
     client.setProject(projectid);
     databases=Databases(client);
   }
 
-  // Future <Document>addTask()async{}
+  Future <Document>addTask(String _task)async{
+    try{
+        final documentId=ID.unique();
+
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        print(_task);
+        print(documentId);
+
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+         
+        final result=await databases.createDocument(databaseId: databaseId, collectionId: collectionId, documentId: documentId,
+         data: {'task':_task,'isCompleted':false});
+         return result;
+    }catch(e){
+      print(e);
+      rethrow;
+    }
+  
+  }
+
+  Future<List<Document>> getTask()async{
+    try {
+      final result=await databases.listDocuments(databaseId: databaseId, collectionId: collectionId);
+      print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      print(result);
+       print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+       return result.documents;
+    } catch (e) {
+      print(e);
+      rethrow;
+      
+    }
+  }
+
+  Future<void>delete(String documentId)async{
+    try {
+      final delete=await databases.deleteDocument(
+        databaseId: databaseId,
+         collectionId: collectionId, 
+         documentId: documentId);
+    } catch (e) {
+      print(e);
+      
+    }
+  }
 }
